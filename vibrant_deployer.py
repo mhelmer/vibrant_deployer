04454ba299aaeaa3370ebc59dc20269e67f73916
Magnus Helmersson <magnus.helmersson@gmail.com>
2015-06-24 19:03:56 +0200

    Merge selected ref instead of origin/master

--- vibrant_deployer.py
+++ vibrant_deployer.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, Response
 import subprocess
 
-from local_settings import SECRET_TOKEN, DEPLOY_TREE, BRANCH, DJANGO_VENV
+from local_settings import SECRET_TOKEN, DEPLOY_TREE, DJANGO_VENV
 
 application = Flask(__name__)
 
@@ -9,11 +9,12 @@ application = Flask(__name__)
 @application.route('/deploy/', methods=['POST'])
 def deploy():
     token = request.json.get('token')
+    ref = request.json.get('ref')
 
     if SECRET_TOKEN == token:
         try:
             subprocess.check_call(['./deploy',
-                                   DEPLOY_TREE, BRANCH, DJANGO_VENV])
+                                   ref, DEPLOY_TREE, DJANGO_VENV])
         except Exception:
             return Response('Deployement failed', 500, )
 
